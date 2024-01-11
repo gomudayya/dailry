d06d14ba9d970c66cf62070f8637f856d2e25c1e
gomudayya <129571789+gomudayya@users.noreply.github.com>
2024-01-11 12:14:53 +0900

    Update JwtUtil.java

--- backend/src/main/java/com/daily/daily/auth/jwt/JwtUtil.java
+++ backend/src/main/java/com/daily/daily/auth/jwt/JwtUtil.java
@@ -120,6 +120,7 @@ public class JwtUtil {
                 .secure(true)
                 .httpOnly(true)
                 .sameSite("None")
+                .domain("https://localhost:3000")
                 .build();
     }
 }
